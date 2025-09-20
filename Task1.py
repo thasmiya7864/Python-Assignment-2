ae285926014ff31f626b60b519e4409227976d08
thasmiya7864 <thasmiya7864@gmail.com>
2025-09-20 22:22:20 +0530

    initial commit

---
+++ Task1.py
@@ -0,0 +1,11 @@
+#Control Strctures Python
+#Check if a number is Even or Odd
+
+num = int(input("Enter a number"))
+if num % 2 == 0:
+    print("it is an even number")
+
+else:
+    print("its is an Odd number")
+    
+    
---
+++ Task2.py
@@ -0,0 +1,7 @@
+'''Sum of integers from 1 to 50 using loop'''
+
+total_sum = 0
+for i in range(1,51):
+    total_sum += i
+
+print("The sum of numbers from 1 to 51 is : ",total_sum)
